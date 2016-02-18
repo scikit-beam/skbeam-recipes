f9da60fada36195b64aca3a351b6411d148120ad
Eric Dill <edill@bnl.gov>
2016-02-18 14:36:36 -0500

    The cli flag for conda-build-all is matrix-conditions

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -51,7 +51,7 @@ unset LANG
 # state the build dependencies at OS level, too.
 yum install -y libXext libXrender libSM tk libX11-devel
 
-conda-build-all /conda-recipes --upload-channels scikit-beam --inspect-channels scikit-beam --build-condition "numpy >=1.8" "python >=2.7,<3|>=3.4"
+conda-build-all /conda-recipes --upload-channels scikit-beam --inspect-channels scikit-beam --matrix-conditions "numpy >=1.8" "python >=2.7,<3|>=3.4"
 
 EOF
 
